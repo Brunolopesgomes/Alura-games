a165472232b38bd0d9c096338ff10d7a0853f60c
Bruno Lopes <137509964+Brunolopesgomes@users.noreply.github.com>
2025-05-28 17:18:32 -0300

    atualização

--- js/app.js
+++ js/app.js
@@ -1,34 +1,28 @@
 function alterarStatus(id){
 
-let gameclicado = document.getElementById(`game-${id}`);
+    let jogo = document.getElementById(`game-${id}`);
+    let imagem = jogo.querySelector('.dashboard__item__img,.dashboard__item__img--rented');
+     let botao =  jogo.querySelector('.dashboard__item__button,.dashboard__item__button--return');
 
-let imagem = gameclicado.querySelector('.dashboard__item__img , .dashboard__item__img--rented ');
-
-let botao = gameclicado.querySelector('.dashboard__item__button, .dashboard__item__button--return');
-
-
-
-if (botao.classList.contains('dashboard__item__button')){
+if( botao.classList.contains('dashboard__item__button')){
     botao.classList.remove('dashboard__item__button');
     botao.classList.add('dashboard__item__button--return');
-    
-}else{
-    botao.classList.remove('dashboard__item__button--return');
-    botao.classList.add('dashboard__item__button');
-   
-}
-if ( botao.textContent == 'Alugar'){
     botao.textContent = 'Devolver';
-}else{
-    botao.textContent = 'Alugar';
-
 }
+else{
+   botao.classList.remove('dashboard__item__button--return');
+    botao.classList.add('dashboard__item__button');
+    botao.textContent = 'Alugar';
+}
+
 if(imagem.classList.contains('dashboard__item__img')){
     imagem.classList.remove('dashboard__item__img');
     imagem.classList.add('dashboard__item__img--rented');
-}else{
+
+}
+else{
     imagem.classList.remove('dashboard__item__img--rented');
-    imagem.classList.add('dashboard__item__img');
+     imagem.classList.add('dashboard__item__img');
 }
 
 }
